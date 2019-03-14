fc1044cda5898fedd8f39c5d7b1fad36eb66d725
Jesse Williamson <jesse@reflexionhealth.com>
2019-03-14 15:34:13 -0700

    more examples

--- code-examples.ts
+++ code-examples.ts
@@ -1,4 +1,4 @@
-const add = (a: number, b: number) => {
+const add = (a: number, b: number): number => {
   return a + b;
 }
 
@@ -6,22 +6,41 @@ const add10 = (a: number): number => {
   return a + 10;
 }
 
-const impureAdd = (a: number, b: number) => {
+const sub5 = (a: number): number => {
+  return a - 5;
+}
+
+const impureAdd = (a: number, b: number): number => {
   console.log("Launching missles");
   return a + b;
 }
 
-type func<A, B> = (a: A) => B;
-type reduceFunc<A, B> = (a: A, b: B) => A;
+const curriedAdd = (a: number) => (b: number): number => {
+  return a + b;
+}
 
-function map<A, B>(f: func<A, B>, xs: A[]): B[] {
+const curriedAdd10 = curriedAdd(10);
+
+type Func<A, B> = (a: A) => B;
+
+type ReduceFunc<A, B> = (a: A, b: B) => A;
+
+function map<A, B>(f: Func<A, B>, xs: A[]): B[] {
   return xs.map(f);
 }
 
-function reduce<A, B>(f: reduceFunc<A, B>, a: A, xs: B[]): A {
+function reduce<A, B>(f: ReduceFunc<A, B>, a: A, xs: B[]): A {
   return xs.reduce(f, a);
 }
 
-function compose<A, B, C>(g: func<B, C>, f: func<A, B>, a: A): C {
-  return g(f(a));
+function compose<A, B, C>(g: Func<B, C>, f: Func<A, B>): Func<A, C> {
+  return (a: A) => {
+    return g(f(a));
+  }
 }
+
+const add10Sub5 = compose(sub5, add10);
+
+add10Sub5(10);
+
+reduce(add, 0, [1, 2, 3]);
